866f69d2e20b3d7ec741f2db987e1eca67a570fb
Ryldon Salvador <50676729+ryldonsalvador@users.noreply.github.com>
2025-03-16 07:18:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("lightboxModal");
+  const openModalBtn = document.getElementById("openModal");
+  const closeModalBtn = document.querySelector(".close");
+  const modalIframe = document.getElementById("modalIframe");
+
+  openModalBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+    document.getElementById("myvideo").pause();
+    modalIframe.src = "./external/lightbox-index.html"; // Load external page
+  });
+
+  closeModalBtn.addEventListener("click", closeLightbox);
+
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      // Close when clicking on overlay
+      closeLightbox();
+    }
+  });
+
+  function closeLightbox() {
+    modal.style.display = "none";
+    modalIframe.src = ""; // Clear src to stop loading
+    document.getElementById("myvideo").play();
+  }
+});
